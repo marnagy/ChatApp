04094a9f3ee13174f1e260b749a05c9e53c05e6a
Marek Nagy <62247071@cuni.cz>
2020-07-07 21:27:10 +0200

    Added Messages and MessageType

---
+++ ChatLib/Messages/IMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatLib.Messages
+{
+	public interface IMessage
+	{
+		void Send();
+	}
+}
---
+++ ChatLib/Messages/TextMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatLib.Messages
+{
+	public class TextMessage : IMessage
+	{
+		public void Send()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
