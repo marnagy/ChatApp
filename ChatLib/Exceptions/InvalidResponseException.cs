4c46c81d4386252279049c5ccc35c63644ec7427
Marek Nagy <62247071@cuni.cz>
2020-07-20 16:49:14 +0200

    Added custom exceptions

---
+++ ChatLib/Exceptions/InvalidResponseException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ChatClient
+{
+	[Serializable]
+	public class InvalidResponseException : Exception
+	{
+		public InvalidResponseException()
+		{
+		}
+
+		public InvalidResponseException(string message) : base(message)
+		{
+		}
+
+		public InvalidResponseException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected InvalidResponseException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
---
+++ ChatLib/Exceptions/InvalidSessionIDException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ChatClient
+{
+	[Serializable]
+	public class InvalidSessionIDException : Exception
+	{
+		public InvalidSessionIDException()
+		{
+		}
+
+		public InvalidSessionIDException(string message) : base(message)
+		{
+		}
+
+		public InvalidSessionIDException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected InvalidSessionIDException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
