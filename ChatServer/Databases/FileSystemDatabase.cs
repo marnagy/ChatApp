22ea296551b7e20ae992d40053ac21e3a0a1cd02
Marek Nagy <62247071@cuni.cz>
2020-07-20 16:48:56 +0200

    Database Update

--- ChatServer/Databases/FileSystemDatabase.cs
+++ ChatServer/Databases/FileSystemDatabase.cs
@@ -4,6 +4,10 @@ using ChatServer.Databases;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ChatServer
 {
@@ -15,32 +19,63 @@ namespace ChatServer
 		private const string chatsDirName = "chats";
 		private const string groupChatsDirName = "group_chats";
 		private const string infoFileName = ".info";
+		private const string chatInfoFileName = ".chInfo";
+		private const string groupChatInfoFileName = ".gchInfo";
 
+		public const char simpleChatSeparator = ' ';
+
+		private const int saltLength = 16;
+		private const int hashKey = 20;
+		private const int hashIterations = 20_000;
 
 		private readonly DirectoryInfo masterDir;
 		private readonly DirectoryInfo usersDir;
 		private readonly DirectoryInfo chatsDir;
 		private readonly DirectoryInfo groupChatsDir;
 
-		private readonly HashSet<string> allUsernames = new HashSet<string>();
-		private readonly HashSet<string> onlineUsers = new HashSet<string>();
+		private readonly RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
+		private readonly SHA512CryptoServiceProvider sha512 = new SHA512CryptoServiceProvider();
+
+
+		private readonly HashSet<Email> allEmails = new HashSet<Email>();
+		private readonly HashSet<Username> allUsernames = new HashSet<Username>();
+		private readonly HashSet<Username> onlineUsers = new HashSet<Username>();
+		//private readonly Dictionary<Username, Email>
 
 		private ulong groupIDGenerator = 0;
 
-		public FileSystemDatabase()
+		private FileSystemDatabase()
 		{
 			masterDir = Directory.CreateDirectory(Path.Combine(masterDirName));
 			usersDir = masterDir.CreateSubdirectory(usersDirName);
-			usersDir = masterDir.CreateSubdirectory(usersDirName);
-			usersDir = masterDir.CreateSubdirectory(usersDirName);
+			chatsDir = masterDir.CreateSubdirectory(chatsDirName);
+			groupChatsDir = masterDir.CreateSubdirectory(groupChatsDirName);
+		}
+		public static FileSystemDatabase Initialize()
+		{
+			var db = new FileSystemDatabase();
+			
 
-			using (var writer = new StreamWriter(File.Create(Path.Combine(masterDir.FullName, infoFileName))))
+			using (var writer = new StreamWriter(File.Create(Path.Combine(db.masterDir.FullName, infoFileName))))
 			{
 				var dt = DateTime.Now;
 				writer.Write($"Created on {dt.Day}.{dt.Month}.{dt.Year} at {dt.Hour}:{dt.Minute}.");
 			}
-		}
 
+			db.LoadUsernamesAndEmails();
+			return db;
+		}
+		private void LoadUsernamesAndEmails()
+		{
+			foreach (DirectoryInfo userDir in usersDir.EnumerateDirectories())
+			{
+				allUsernames.Add(userDir.Name.ToUsername());
+				using (var reader = new StreamReader(File.OpenRead(Path.Combine(userDir.FullName, infoFileName))))
+				{
+					allEmails.Add(reader.ReadLine().ToEmail());
+				}
+			}
+		}
 		private bool _initialized = false;
 		public bool initialized { get => _initialized; }
 
@@ -55,11 +90,6 @@ namespace ChatServer
 			throw new NotImplementedException();
 		}
 
-		public bool AddUser(string username, char[] password, string email)
-		{
-			throw new NotImplementedException();
-		}
-
 		public bool Connect()
 		{
 			if (Directory.Exists(Path.Combine(".", masterDirName)) && Directory.Exists(Path.Combine(".", masterDirName, usersDirName))
@@ -69,43 +99,82 @@ namespace ChatServer
 			}
 			else
 			{
-				Initialize();
+				Init();
 			}
 			return true;
 		}
 
-		public string CreateChat(User user1, User user2)
+		public (bool success, string ReasonOrName) CreateChat(User user1, User user2, IMessage msg)
 		{
-			if (string.Compare(user1.username, user2.username) > 0)
+			string chatName = GetChatName(user1, user2);
+			lock (this)
 			{
-				var info = Directory.CreateDirectory(Path.Combine(".",
-					masterDirName, chatsDirName, user1.username + "_" + user2.username));
-				return user1.username + "_" + user2.username;
+				string simpleChatName;
+				foreach (var item in File.ReadLines(Path.Combine(
+					usersDir.FullName, user1.username.ToString(), chatInfoFileName)))
+				{
+					(simpleChatName, _) = GetSimpleChatInfo(item);
+					if (chatName == simpleChatName)
+					{
+						return (false, "Chat already exists.");
+					}
+				}
+
+				var chatDirInfo = chatsDir.CreateSubdirectory(chatName);
+
+				using (var writer = File.CreateText(Path.Combine(chatDirInfo.FullName, infoFileName)))
+				{
+					var dt = DateTime.UtcNow;
+					writer.WriteLine(dt);
+					writer.write
+				}
+
 			}
-			return user2.username + "_" + user1.username;
+			return (true,chatName);
+		}
+		private (string name, long messageID) GetSimpleChatInfo(string line)
+		{
+			if (line == null) throw new ArgumentNullException();
+			string[] lineParts = line.Split(simpleChatSeparator);
+			if (long.TryParse(lineParts[2], out long lastMsgID))
+			{
+				return (lineParts[0] + simpleChatSeparator.ToString() + lineParts[1], lastMsgID);
+			}
+			else
+			{
+				throw new FormatException("Wrong format of stored info about chat.");
+			}
+		}
+		private string GetChatName(User user1, User user2)
+		{
+			if (user1.username.CompareTo(user2.username) > 0)
+			{
+				return user1.username + simpleChatSeparator.ToString() + user2.username;
+			}
+			return user2.username + simpleChatSeparator.ToString() + user1.username;
 		}
 
 		public string CreateChat(User[] users)
 		{
-			ulong groupID = groupIDGenerator++;
-			var groupDir = groupChatsDir.CreateSubdirectory(groupID.ToString());
-			using (var writer = File.CreateText(Path.Combine(groupDir.FullName, infoFileName)))
-			{
-				foreach(User user in users)
-				{
-					writer.WriteLine(user.username);
-				}
-			}
+			//ulong groupID = groupIDGenerator++;
+			//var groupDir = groupChatsDir.CreateSubdirectory(groupID.ToString());
+			//using (var writer = File.CreateText(Path.Combine(groupDir.FullName, infoFileName)))
+			//{
+			//	foreach(User user in users)
+			//	{
+			//		writer.WriteLine(user.username);
+			//	}
+			//}
 
-
+			throw new NotImplementedException();
 		}
 
-		public IEnumerable<string> GetUsers()
+		public IEnumerable<Username> GetUsers()
 		{
 			return allUsernames;
 		}
 
-		public bool Initialize()
+		public bool Init()
 		{
 			if (_initialized) return true;
 			Directory.CreateDirectory(Path.Combine(masterDirName, usersDirName));
@@ -126,5 +195,124 @@ namespace ChatServer
 		{
 			return onlineUsers.Add(user.username);
 		}
+
+		public void Run()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(Email email)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void CreateGroupChatList(DirectoryInfo userDir)
+		{
+			File.CreateText(Path.Combine(userDir.FullName, groupChatInfoFileName));
+		}
+
+		private void CreateChatList(DirectoryInfo userDir)
+		{
+			File.CreateText(Path.Combine(userDir.FullName, chatInfoFileName));
+		}
+
+		private void CreateUserInfo(DirectoryInfo userDir, Email email, Password password)
+		{
+			using (var writer = new StreamWriter(File.OpenWrite(Path.Combine(userDir.FullName, infoFileName))))
+			{
+				// store email
+				writer.WriteLine(email.ToString());
+
+				// generate hash of password + salt
+				byte[] salt = new byte[saltLength];
+				random.GetBytes(salt);
+
+				//var pbkdf2 = new Rfc2898DeriveBytes(password.ToString(), salt, hashIterations);
+				//byte[] hash = pbkdf2.GetBytes(hashKey);
+				byte[] hash = new Rfc2898DeriveBytes(password.ToString(),
+					salt, hashIterations).GetBytes(hashKey);
+
+				byte[] hashBytes = new byte[saltLength + hashKey];
+				Array.Copy(salt, 0, hashBytes, 0, saltLength);
+				Array.Copy(hash, 0, hashBytes, saltLength, hashKey);
+
+				// store hash
+				string savedPasswordHash = Convert.ToBase64String(hashBytes);
+				writer.WriteLine(savedPasswordHash);
+			}
+
+			//set file to read-only
+		}
+		public (bool successful, string reasonOfFail) SignIn(Username username, Password password)
+		{
+			const string signInFailMssg = "Username or password is incorrect.";
+			lock (this)
+			{
+				if (allUsernames.Contains(username))
+				{
+					string[] lines = File.ReadAllLines(Path.Combine(usersDir.FullName, username.ToString(), infoFileName));
+					// load hash
+					string savedPasswordHash = lines[1];
+					lines = null;
+					// convert hash to byte[]
+					byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+					// extract salt
+					byte[] salt = new byte[saltLength];
+					Array.Copy(hashBytes, 0, salt, 0, saltLength);
+
+					byte[] hash = new Rfc2898DeriveBytes(password.ToString(),
+						salt, hashIterations).GetBytes(hashKey);
+					for (int i = 0; i < hashKey; i++)
+					{
+						if (hashBytes[i+saltLength] != hash[i])
+						{
+							return (false, signInFailMssg);
+						}
+					}
+					return (true, "");
+				}
+				else
+				{
+					return (false, signInFailMssg);
+				}
+			}
+		}
+
+		public bool Contains(Username username)
+		{
+			return allUsernames.Contains(username);
+		}
+
+		public (bool successful, string reasonOfFail) AddUser(Email email, Username username, Password password)
+		{
+			lock (this)
+			{
+				if (!allUsernames.Contains(username))
+				{
+					if (!allEmails.Contains(email))
+					{
+						var userDir = usersDir.CreateSubdirectory(username.ToString());
+					
+						CreateUserInfo(userDir, email, password);
+						CreateChatList(userDir);
+						CreateGroupChatList(userDir);
+
+						allUsernames.Add(username);
+						allEmails.Add(email);
+						return (true, "");
+					}
+					else
+					{
+						return (false, "Email is already signed up.");
+					}
+				}
+				else
+				{
+					return (false, "Username already taken. Please, try different username.");
+				}
+			}
+		}
+
+
 	}
 }
--- ChatServer/Databases/IDatabase.cs
+++ ChatServer/Databases/IDatabase.cs
@@ -9,23 +9,34 @@ namespace ChatServer.Databases
 	interface IDatabase
 	{
 		bool initialized { get; }
-		bool AddUser(string username, char[] password);
+		(bool successful, string reasonOfFail) AddUser(Email email, Username username, Password password);
+		(bool successful, string reasonOfFail) SignIn(Username username, Password password);
 		bool MakeOnline(User user);
 		bool AddMessage(ChatInfo info, IMessage message);
 		/// <summary>
-		/// Method to initialize simple chat of 2 users.
+		/// Creates new chat of 2 users
 		/// </summary>
-		/// <param name="user1">User initializing conversation.</param>
-		/// <param name="user2">User who receives first message.</param>
-		/// <returns>Result if creation was successful.</returns>
-		string CreateChat(User user1, User user2);
+		/// <param name="user1"></param>
+		/// <param name="user2"></param>
+		/// <returns>ValueTuple of success and name of created chat if success.</returns>
+		(bool success, string reason) CreateChat(User user1, User user2, IMessage msg);
 		/// <summary>
 		/// Method to initialize group chat.
 		/// </summary>
 		/// <param name="users">Participants of the group chat.</param>
-		/// <returns>Result if creation was successful.</returns>
+		/// <returns>Name of chat identifier.</returns>
 		string CreateChat(User[] users);
-		bool Contains(User user);
-		IEnumerable<string> GetUsers();
+		/// <summary>
+		/// Checks if there exists user with the given username
+		/// </summary>
+		/// <param name="username"></param>
+		/// <returns>If there is a user with given username.</returns>
+		bool Contains(Username username);
+		/// <summary>
+		/// Checks if there exists user with the given email address.
+		/// </summary>
+		/// <param name="email"></param>
+		/// <returns>If there is a user with given email address.</returns>
+		bool Contains(Email email);
 	}
 }
