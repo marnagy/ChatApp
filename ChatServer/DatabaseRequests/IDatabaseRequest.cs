5f67efd0ff996d53254b3d430b64a4afc08ebad8
Marek Nagy <62247071@cuni.cz>
2020-07-20 16:50:17 +0200

    Added Database Requests

---
+++ ChatServer/DatabaseRequests/IDatabaseRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer.DatabaseRequests
+{
+	interface IDatabaseRequest
+	{
+		DatabaseRequestType type {get;}
+	}
+}
