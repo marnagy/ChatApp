a155684e251ef7e9b2da53587c0510c0802b141f
Marek Nagy <62247071@cuni.cz>
2020-07-20 17:23:42 +0200

    Added needed data for classes

--- ChatLib/ChatInfo.cs
+++ ChatLib/ChatInfo.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ChatLib
 {
-	public class ChatInfo
+	public class ChatInfo : ISendable
 	{
-		readonly ChatType type;
-		readonly int chatID;
+		public readonly ChatType type;
+		public readonly string chatID;
+		public readonly Username[] participants;
+		public ChatInfo(ChatType type, string chatID, Username[] participants)
+		{
+			if (participants == null) throw new ArgumentNullException();
+			this.type = type;
+			this.chatID = chatID;
+			this.participants = participants;
+		}
 
+		public void Send(BinaryWriter writer)
+		{
+			lock (writer)
+			{
+				writer.Write(type.ToString());
+				writer.Write(chatID);
+				writer.Write(participants.Length);
+				foreach (var username in participants)
+				{
+					writer.Write(username.ToString());
+				}
+
+				writer.Flush();
+			}
+		}
 	}
 }
--- ChatLib/Responses/AccountInfoResponse.cs
+++ ChatLib/Responses/AccountInfoResponse.cs
@@ -7,11 +7,38 @@ namespace ChatLib.Responses
 {
 	public struct AccountInfoResponse : IResponse
 	{
+		private const ResponseType type = ResponseType.AccountInfo;
 		private readonly long sessionID;
-		
+		public readonly ChatInfo[] simpleChats;
+		public readonly ChatInfo[] groupChats;
+		public AccountInfoResponse(ChatInfo[] simpleChats, ChatInfo[] groupChats, long sessionID)
+		{
+			if (simpleChats == null || groupChats == null) throw new ArgumentNullException();
+			this.sessionID = sessionID;
+			this.simpleChats = simpleChats;
+			this.groupChats = groupChats;
+		}
 		public void Send(BinaryWriter writer)
 		{
-			throw new NotImplementedException();
+			lock (writer)
+			{
+				writer.Write(sessionID);
+				writer.Write(type.ToString());
+
+				writer.Write(simpleChats.Length);
+				for (int i = 0; i < simpleChats.Length; i++)
+				{
+					simpleChats[i].Send(writer);
+				}
+
+				writer.Write(groupChats.Length);
+				for (int i = 0; i < groupChats.Length; i++)
+				{
+					groupChats[i].Send(writer);
+				}
+
+				writer.Flush();
+			}
 		}
 	}
 }
