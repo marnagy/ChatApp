4a27f3cb81821455479b4054aed66e2645da0657
Marek Nagy <62247071@cuni.cz>
2020-08-19 18:44:42 +0200

    Added BinaryFormatter Wrappers

---
+++ ChatLib/BinaryFormatters/BinaryFormatterReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ChatLib.BinaryFormatters
+{
+	class BinaryFormatterReader
+	{
+		private readonly BinaryFormatter deformatter;
+		private readonly Stream stream;
+		public bool IsClosed { get; private set;} = false;
+		public bool IsEmpty => stream.Length == 0;
+		public BinaryFormatterReader(BinaryFormatter bf, Stream stream)
+		{
+			if (!stream.CanWrite) throw new ArgumentException("Stream cannot be written to.");
+			deformatter = bf;
+		}
+
+		public object Read()
+		{
+			return deformatter.Deserialize(stream);
+		}
+		
+		public bool Close()
+		{
+			lock (stream)
+			{
+				if (!IsClosed)
+				{
+					if (stream.Length == 0) {
+						stream.Close();
+						IsClosed = true;
+						return true;
+					}
+					else
+						return false;
+				}
+				else
+					return true;
+			}
+		}
+	}
+}
---
+++ ChatLib/BinaryFormatters/BinaryFormatterWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ChatLib.BinaryFormatters
+{
+	class BinaryFormatterWriter
+	{
+		private readonly BinaryFormatter formatter;
+		private readonly Stream stream;
+		public bool IsClosed { get; private set;} = false;
+		public BinaryFormatterWriter(BinaryFormatter bf, Stream stream)
+		{
+			if (!stream.CanWrite) throw new ArgumentException("Stream cannot be written to.");
+			formatter = bf;
+		}
+
+		public void Write(object obj)
+		{
+			if (obj == null) throw new ArgumentNullException();
+			formatter.Serialize(stream, obj);
+		}
+
+		public bool Close()
+		{
+			lock (stream)
+			{
+				if (!IsClosed)
+				{
+					if (stream.Length == 0) {
+						stream.Close();
+						IsClosed = true;
+						return true;
+					}
+					else
+						return false;
+				}
+				else
+					return true;
+			}
+		}
+	}
+}
