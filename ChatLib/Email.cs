be278f2670108e3303f574dda1c02263ae2204aa
Marek Nagy <62247071@cuni.cz>
2020-08-20 16:09:00 +0200

    Made helping Wrappers serializable

--- ChatLib/Email.cs
+++ ChatLib/Email.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Net.Mail;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace ChatLib
 {
-	public struct  Email
+	[Serializable]
+	public struct  Email : ISerializable
 	{
+		const string ValueSerializationName = "E-mail";
 		private readonly MailAddress Address;
 		public Email(string value)
 		{
 			Address = new MailAddress(value);
+		}
+		public Email(SerializationInfo info, StreamingContext context)
+		{
+			Address = new MailAddress( (string)info.GetValue( ValueSerializationName, typeof(string) ) );
+		}
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue( ValueSerializationName, Address.ToString());
 		}
 		public override string ToString()
 		{
