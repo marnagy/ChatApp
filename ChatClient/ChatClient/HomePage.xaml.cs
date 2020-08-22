6f2c7813eaad957f940fbb23dc7db88e9a29ca3e
Marek Nagy <62247071@cuni.cz>
2020-08-22 20:57:56 +0200

    [WIP] HomePage changes

--- ChatClient/ChatClient/HomePage.xaml.cs
+++ ChatClient/ChatClient/HomePage.xaml.cs
@@ -18,7 +18,8 @@ namespace ChatClient
 	{
 		private readonly App app;
 
-		private ObservableCollection<ChatViewCell> chats = new ObservableCollection<ChatViewCell>();
+		private readonly ObservableCollection<TextCell> chatViews = new ObservableCollection<TextCell>();
+		private readonly List<ChatInfo> chats = new List<ChatInfo>();
 
 		public HomePage(App app, AccountInfoResponse aiResp)
 		{
@@ -26,14 +27,50 @@ namespace ChatClient
 			InitializeComponent();
 
 			LoadChats(aiResp.SimpleChats, aiResp.GroupChats);
+			listView.ItemsSource = chats;
 		}
 
 		private void LoadChats(ChatInfo[] simpleChats, ChatInfo[] groupChats)
 		{
-			throw new NotImplementedException();
+			foreach (var chat in simpleChats)
+			{
+				chats.Add(chat);
+			}
+
+			foreach (var chat in groupChats)
+			{
+				chats.Add(chat);
+			}
+
+			chats.Sort((a,b) => DateTime.Compare(a.lastMessageTime, b.lastMessageTime));
+
+			UpdateChatsView(chatViews, chats);
+		}
+
+		private void UpdateChatsView(ObservableCollection<TextCell> chatViews, List<ChatInfo> chats)
+		{
+			chatViews.Clear();
+
+			foreach (var item in chats)
+			{
+				chatViews.Add( new TextCell{ Text = item.Name, Detail = item.ID.ToString() } );
+			}
 		}
 
 		private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			lock (chats)
+			{
+				DisplayAlert("Cell tapped!", "Cell: " + chats[e.ItemIndex].ID, "Okay");
+			}
+		}
+
+		private void newChat_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void newGroupChat_Click(object sender, EventArgs e)
 		{
 
 		}
