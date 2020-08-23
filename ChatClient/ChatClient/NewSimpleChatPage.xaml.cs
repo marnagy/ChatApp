6dcb5f9f9428a11dcb979029bc5f5f55218bed0c
Marek Nagy <62247071@cuni.cz>
2020-08-24 00:23:14 +0200

    Added new Page for Creating new simple chat

---
+++ ChatClient/ChatClient/NewSimpleChatPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewSimpleChatPage : ContentPage
+	{
+		public NewSimpleChatPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_1(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
