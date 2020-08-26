e6b9f903331c20143d1ffcfacf4309e4ffd63713
Marek Nagy <62247071@cuni.cz>
2020-08-26 21:52:36 +0200

    Added class for ChatPage

---
+++ ChatClient/ChatClient/ChatPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChatPage : ContentPage
+	{
+		private readonly App app;
+		public ChatPage(App app)
+		{
+			this.app = app;
+			InitializeComponent();
+		}
+	}
+}
