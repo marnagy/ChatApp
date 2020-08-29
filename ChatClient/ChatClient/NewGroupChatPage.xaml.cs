00893d2e5529fca07a74616e0abe9be6bdf2ad79
Marek Nagy <62247071@cuni.cz>
2020-08-29 14:42:09 +0200

    NewGroupChat Page stuff

--- ChatClient/ChatClient/NewGroupChatPage.xaml.cs
+++ ChatClient/ChatClient/NewGroupChatPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ChatLib;
 using ChatLib.BinaryFormatters;
+using ChatLib.Requests;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,18 +19,22 @@ namespace ChatClient
 		Username myUsername;
 		BinaryFormatterWriter writer;
 		long sessionID;
-		private readonly ObservableCollection<string> collection = new ObservableCollection<string>();
+		private readonly ObservableCollection<StringCell> collection = new ObservableCollection<StringCell>();
 		public NewGroupChatPage(Username myUsername, BinaryFormatterWriter writer, long sessionID)
 		{
 			this.myUsername = myUsername;
 			this.writer = writer;
 			this.sessionID = sessionID;
 
+			collection.Add( new StringCell(){ value = string.Empty} );
+			collection.Add( new StringCell(){ value = string.Empty} );
+
 			InitializeComponent();
+			list.ItemsSource = collection;
 		}
 		private void AddEntry(object sender, EventArgs e)
 		{
-
+			collection.Add( new StringCell(){ value = string.Empty} );
 		}
 		private void RemoveEntry(object sender, EventArgs e)
 		{
@@ -37,13 +42,25 @@ namespace ChatClient
 		}
 		private void CreateGroupChat(object sender, EventArgs e)
 		{
-			var users = new List<Username>(collection.Count + 1);
-			users[0] = myUsername;
+			var users = new List<Username>(collection.Count);
+			Navigation.PopModalAsync(animated: true);
 
-			for (int i = 0; i < collection.Count; i++)
-			{
+			Task.Run( () => {
+				string val;
+				for (int i = 0; i < collection.Count; i++)
+				{
+					val = collection[i].value.Trim();
+					if ( val.Length == 0)
+					{
+						Device.InvokeOnMainThreadAsync( () => DisplayAlert("Error", "No username has empty username.", "Okay") );
+						users = null;
+						return;
+					}
+					users.Add( val.ToUsername() );
+				}
 
-			}
+				writer.Write( new NewChatRequest( myUsername, users.ToArray(), sessionID) );
+			});
 		}
 	}
 }
