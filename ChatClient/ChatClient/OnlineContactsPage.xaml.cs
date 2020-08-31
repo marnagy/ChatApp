98917be9e30bac012332d21e7e662ad5e228386e
Marek Nagy <62247071@cuni.cz>
2020-09-01 00:24:21 +0200

    Added Online Contacts Page

---
+++ ChatClient/ChatClient/OnlineContactsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ChatLib;
+
+namespace ChatClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OnlineContactsPage : ContentPage
+	{
+		//private ObservableCollection<StringCell> onlineContacts;
+
+		public OnlineContactsPage(ObservableCollection<StringCell> onlineContacts)
+		{
+			//this.onlineContacts = onlineContacts;
+			InitializeComponent();
+			listView.ItemsSource = onlineContacts;
+		}
+	}
+}
